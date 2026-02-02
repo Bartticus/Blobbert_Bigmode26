c5abfab095db753dc34fe04b1cce0a2b16c11ccf
Bartticus <kbartter@gmail.com>
2026-02-02 11:16:57 -0800

    Anchors and areas captured on ready

--- Scenes/Levels/level_1.gd
+++ Scenes/Levels/level_1.gd
@@ -1,15 +1,27 @@
 extends Node2D
 
 @export var camera: Camera2D
-@export var anchors: Array[Marker2D]
-@export var camera_trans_areas: Array[Area2D]
 @export var current_anchor: Marker2D:
 	set(value):
 		current_anchor = value
 		camera.global_position = current_anchor.global_position
 
+@onready var trans_areas_node: Node2D = %CameraTransitionAreas
+@onready var anchors_node: Node2D = %Anchors
+
+var anchors: Array[Marker2D]
+var camera_trans_areas: Array[Area2D]
+
 
 func _ready() -> void:
+	for child in trans_areas_node.get_children():
+		if child is Area2D:
+			camera_trans_areas.append(child)
+	
+	for child in anchors_node.get_children():
+		if child is Marker2D:
+			anchors.append(child)
+	
 	for area in camera_trans_areas:
 		area.connect("area_exited", _on_screen_area_exited)
 
