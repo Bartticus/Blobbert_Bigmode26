40197310db52568cf84828760d44d566cacf3871
Bartticus <kbartter@gmail.com>
2026-02-02 14:31:07 -0800

    Tilemap is no more

--- Scenes/Levels/level_script.gd
+++ Scenes/Levels/level_script.gd
@@ -2,16 +2,16 @@ class_name LevelScript
 extends Node2D
 
 @onready var camera: Camera2D = %Camera
-@export var current_anchor: Marker2D:
-	set(value):
-		current_anchor = value
-		camera.global_position = current_anchor.global_position
-
 @onready var trans_areas_node: Node2D = %CameraTransitionAreas
 @onready var anchors_node: Node2D = %Anchors
 
 var anchors: Array[Marker2D]
 var camera_trans_areas: Array[Area2D]
+
+@export var current_anchor: Marker2D:
+	set(value):
+		current_anchor = value
+		camera.global_position = current_anchor.global_position
 
 
 func _ready() -> void:
