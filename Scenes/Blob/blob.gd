41f50b62cf56ebee3a4ca1be79f4868927c9eaed
Bartticus <kbartter@gmail.com>
2026-02-02 17:51:10 -0800

    First lab scene, greasable objects can be glass now

--- Scenes/Blob/blob.gd
+++ Scenes/Blob/blob.gd
@@ -22,7 +22,6 @@ func _ready() -> void:
 	for child in softbody.get_children():
 		if child is Bone:
 			bodies.append(child)
-	print(bodies)
 	
 	for child in face_pivot.get_children():
 		if child is Sprite2D:
@@ -64,7 +63,7 @@ func _process(_delta: float) -> void:
 		get_tree().reload_current_scene()
 
 
-func _on_blob_level_transition_area_area_exited(area: Area2D) -> void:
+func _on_blob_level_transition_area_area_exited(_area: Area2D) -> void:
 	for overlapping_area in blob_level_transition_area.get_overlapping_areas():
 		if overlapping_area.name == 'ScreenSpace':
 			Global.level.current_anchor = overlapping_area.get_owner().screen_anchor
--- Scenes/Screens/glass_panes.gd
+++ Scenes/Screens/glass_panes.gd
@@ -6,5 +6,16 @@ extends Node2D
 @export var left_thick_wall: StaticBody2D
 @export var right_thick_wall: StaticBody2D
 
-func _process(_delta: float) -> void:
-	pass
+func _ready() -> void:
+	for pane in left_glass_panes:
+		pane.connect("tree_exiting", remove_left_wall)
+	for pane in right_glass_panes:
+		pane.connect("tree_exiting", remove_right_wall)
+
+func remove_left_wall() -> void:
+	if left_thick_wall:
+		left_thick_wall.queue_free()
+
+func remove_right_wall() -> void:
+	if right_thick_wall:
+		right_thick_wall.queue_free()
