71af681a82b20c9654a25b2e52fd1c0f0bb6e7b4
Bartticus <kbartter@gmail.com>
2026-02-05 11:20:10 -0800

    Starting lab sequence finished

--- Scenes/WindGust/wind_gust.gd
+++ Scenes/WindGust/wind_gust.gd
@@ -55,8 +55,8 @@ func flip_path():
 		$Path2D.curve.add_point(point_pos[idx], -point_in[idx], -point_out[idx], jdx)
 		jdx += 1
 	
-func move_path(delta):
-	for pf in pf_dict:		
+func move_path(_delta):
+	for pf in pf_dict:
 		pf.trail_offset += trail_speed
 		if pf.trail_offset >= 0.0 and pf.trail_offset <= 1.0:
 			pf.progress_ratio = pf.trail_offset
